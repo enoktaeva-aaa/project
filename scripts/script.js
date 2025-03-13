65e90e4a71b3828125d18ca8a9d625bf78c52f4b
enoktaeva-aaa <gertsrita@gmail.com>
2025-03-13 16:42:49 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,46 @@
 'use strict'
 
 document.addEventListener("DOMContentLoaded", () => {
-    
-    const menuActiv = document.querySelector('.menuActiv');       // создаем переменную находя блок по классу
 
-    if (menuActiv) {                                           // проверяем существование элемента в DOM
-        console.log('Константа header существует');
+    const aboutImage = document.querySelector('.about__image');  // создаем переменную находя блок по классу
+    if (aboutImage) {                                           // проверяем существование элемента в DOM
+        console.log('Константа about__image существует');
+        const aboutDescription = document.querySelector('.about__desctription');
+        /* 1. Исключение накладывания контента на хедер при скроле/прокрутке страницы */
+
+        /* 
+         *   Алгоритм
+         *
+         *   1. Начало.
+         *   2. Находим кнопку Оставить заявку
+         *   3. Проверка условия нажата ли кнопка Оставить заявку(навешиваем слушатель событий на кнопку страницы и ожидаем ответа): если конпка нажата.
+         *       3.1. Да: Получаем модальное окно с заявкой (создание переменной, которая будет меняться).
+         *         3.1.1.2. Нет (если кнопка не нажата)
+         *   4. Конец
+         * 
+         *   Блок-схема: /images/блок схема.png
+         */
 
 
-/* 1. Исключение накладывания контента на хедер при скроле/прокрутке страницы */
+        aboutImage.addEventListener('mouseenter', () => {
+            aboutImage.style.opacity = 0.5;
+            // aboutDescription.removeAttribute('hidden');
+            aboutDescription.style.opacity = 1;
+            aboutDescription.style.visibility = "visible";
 
-       /* 
-        *   Алгоритм
-        *
-        *   1. Начало.
-        *   2. Находим кнопку Оставить заявку
-        *   3. Проверка условия нажата ли кнопка Оставить заявку(навешиваем слушатель событий на кнопку страницы и ожидаем ответа): если конпка нажата.
-        *       3.1. Да: Получаем модальное окно с заявкой (создание переменной, которая будет меняться).
-        *         3.1.1.2. Нет (если кнопка не нажата)
-        *   4. Конец
-        * 
-        *   Блок-схема: /images/блок схема.png
-        */
-       }
+
+        });
+
+        aboutImage.addEventListener('mouseleave', () => {
+            aboutImage.style.opacity = 1;
+            // aboutDescription.setAttribute('hidden', true);
+            aboutDescription.style.opacity = 0;
+            aboutDescription.style.visibility = "hidden";
+        });
+
+
+
+
+    }
 }
 );
