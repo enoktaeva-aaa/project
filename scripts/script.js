c5b16965de8094519bf92c320c8ab043c52dde3c
enoktaeva-aaa <gertsrita@gmail.com>
2025-04-03 18:21:43 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -58,55 +58,107 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         });
     }
+    /*3 Формируем объект из элементов цена  и выводим блок динамически*/
+    //Объявляем переменную cardsPrice и сохраняем в нее элемент с классом price
+    const cardsPrice = document.querySelectorAll('.price__price');
+    console.log(cardsPrice);
+    // Если такой элемент существует
+    if (cardsPrice) {
 
-
-
-}
-);
-//Объявляем переменную cardsPrice и сохраняем в нее элемент с классом price
-const cardsPrice = document.querySelectorAll('.price__price');
-console.log(cardsPrice);
-// Если такой элемент существует
-if (cardsPrice) {
-
-//Создаем объект cardsPriceData, которая содержит данные для трех карточки.
+        //Создаем объект cardsPriceData, которая содержит данные для трех карточки.
         const cardsPriceData = {
-// каждая ссылка содержит level (название тарифа), price (цена), description (описание тарифа), button (кнопку для оформления заявки).
+            // каждая ссылка содержит level (название тарифа), price (цена), description (описание тарифа), button (кнопку для оформления заявки).
             0: {
                 weight: '100 грамм',
                 price: '350 ₽',
-               
+
             },
             1: {
                 weight: '1 шт',
                 price: '300 ₽',
-               
+
             },
             2: {
                 weight: '1 шт',
                 price: '260 ₽',
-              
+
+            },
+            3: {
+                weight: '1 шт',
+                price: '260 ₽',
+
             }
         }
 
-//Создаем функцию createCard, которая будет добавлять карточку. Внутри функции 4 переменные: level (название тарифа), price (цена), description (описание тарифа), button (кнопку для оформления заявки)
+        //Создаем функцию createCard, которая будет добавлять карточку. Внутри функции 4 переменные: level (название тарифа), price (цена), description (описание тарифа), button (кнопку для оформления заявки)
         const createCard = (weight, price) => {
-// Создаем переменную  card, которая будет содержать HTML-код карточки и вставляем туда 4 переменные
+            // Создаем переменную  card, которая будет содержать HTML-код карточки и вставляем туда 4 переменные
             const card = `
         цена ${price} за ${weight}
            
         `;
-//  Возвращаем значение переменной card
+            //  Возвращаем значение переменной card
             return card;
         }
-// Создаем цикл for и проходим по всем элементам объекта cardsPriceData.
+        // Создаем цикл for и проходим по всем элементам объекта cardsPriceData.
         for (const cardKey in cardsPriceData) {
-//Получаем данные одной карточки из объекта cardsPriceData 
+            //Получаем данные одной карточки из объекта cardsPriceData 
             const card = cardsPriceData[cardKey];
-//создаем переменную cardElement и вызываем функцию createLink, куда передаем тариф, цену, описание и кнопку (то, из чего будет состоять ваша карточка).
+            //создаем переменную cardElement и вызываем функцию createLink, куда передаем тариф, цену, описание и кнопку (то, из чего будет состоять ваша карточка).
             const cardElement = createCard(card.weight, card.price);
 
-// с помощью метода insertAdjacentHTML добавляем созданный HTML-код в конец списка priceList.
-cardsPrice[cardKey].innerHTML =  cardElement;
+            // с помощью метода insertAdjacentHTML добавляем созданный HTML-код в конец списка priceList.
+            cardsPrice[cardKey].innerHTML = cardElement;
         }
-}
+    }
+    /*4 Преобразуем данные из предыдущего задания в файл json и выводим их с помощью fetch*/
+    const programsContainer = document.querySelector('.programs');
+    if (programsContainer) {
+        const programList = programsContainer.querySelector('.programs__list');
+
+        // Пример URL для получения данных с сервера
+        const apiUrl = 'data.json';
+
+        // Функция для создания карточки
+        const  createProgram = (image, title, description) => {
+
+            // Шаблонные строки и подстановки
+            const card = `
+                <li class="programs__kard">
+                <img class="programs__image" src="${image}">
+                <h3 class="programs__name">${title}</h3>
+                    <p class="programs__desk">${description}</p>
+                </li>
+            `;
+
+            return card;
+        }
+
+        // Загрузка данных с сервера
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data); // Данные
+                console.log(typeof (data)); // Тип полученных данных
+
+                // for (const item in data) {
+                //     const card = data[item];
+
+                //     const cardElement = createCard(card.link, card.icon, card.iconAlt, card.iconWidth, card.iconHeight, card.title, card.description);
+                //     cardList.insertAdjacentHTML('beforeend', cardElement);
+                // }
+
+                data.forEach(item => {
+                    const cardElement = createProgram(item.image, item.title, item.description);
+                    programList.insertAdjacentHTML('beforeend', cardElement);
+                });
+            })
+            .catch(error => {
+                console.error('Ошибка при загрузке данных:', error);
+            });
+    }
+
+});
+
+
+
