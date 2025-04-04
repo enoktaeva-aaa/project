1307655138cd846a13ddc66554d87f0af868b7ee
enoktaeva-aaa <gertsrita@gmail.com>
2025-04-04 17:25:48 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -158,6 +158,30 @@ document.addEventListener("DOMContentLoaded", () => {
             });
     }
 
+
+    //7.
+    const slider = document.querySelector('.swiper');
+
+    if (slider) {
+        const swiper = new Swiper(slider, {
+            // Дополнительные параметры
+            slidesPerView: 3, // Количество слайдов на экране
+            spaceBetween: 40, // Расстояние между слайдами
+            loop: true,  // Зацикливание слайдов
+
+            // Пагинация
+            pagination: {
+                el: '.swiper-pagination',
+            },
+
+            // Навигационные стрелки
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        });
+    }
+
 });
 
 
